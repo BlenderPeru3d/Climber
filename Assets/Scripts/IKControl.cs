341795897d9b634fa0a6eaef827c3b09a2b36e20
BlenderPeru3d <peru3dblender@gmail.com>
2020-07-27 18:54:03 -0500

    initial

---
+++ Assets/Scripts/IKControl.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IKControl : MonoBehaviour
+{
+    protected Animator animator;
+
+    [SerializeField]
+    private bool ikActive;
+    [SerializeField]
+    private Transform TargetManoDerecha = null;
+    [SerializeField]
+    [Range(0,1)]
+    private float PesoManoDerecha;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+    private void OnAnimatorIK()
+    {
+        if (animator)
+        {
+            if (ikActive)
+            {
+                if (TargetManoDerecha != null)
+                {
+                    animator.SetIKPositionWeight(AvatarIKGoal.RightHand, PesoManoDerecha);
+                    animator.SetIKPosition(AvatarIKGoal.RightHand, TargetManoDerecha.position);
+                }
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/SetMaterials.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetMaterials : MonoBehaviour
+{
+    [SerializeField]
+    private Vector3[] vertices;
+    public Vector3[] Vertices
+    {
+        get { return vertices; }
+        set { vertices = value; }
+    }
+    [SerializeField]
+    private int[] triangles;
+    public int[] Triangles
+    {
+        get { return triangles; }
+        set { triangles = value; }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
